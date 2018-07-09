ec3a0b3429359f0b3f33acd3b9d37fe493bfc417
Bob Bao <baoqiyuan777@gmail.com>
2018-07-09 05:07:53 -0400

    fix: add timestamp to reduce tweet duplication error

--- src/services/post_rankings_services/tweetRankingService.js
+++ src/services/post_rankings_services/tweetRankingService.js
@@ -11,6 +11,7 @@ module.exports = {
         + "1." + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.monthly.rank1.title) + "\n" + processedRanking.monthly.rank1.uri + "\n" 
         + "2." + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.monthly.rank2.title) + "\n" + processedRanking.monthly.rank2.uri + "\n" 
         + "3." + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.monthly.rank3.title) + "\n" + processedRanking.monthly.rank3.uri + '\n'
+        + moment().tz("Asia/Tokyo").format("YYYY-MM-DD-HH");
 
         tweetPostStatUpdateService.tweetPostStatUpdate(monthlyTweet, "Monthly");
     },
@@ -20,6 +21,7 @@ module.exports = {
         + "1." + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.weekly.rank1.title) + "\n" + processedRanking.weekly.rank1.uri + "\n" 
         + "2." + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.weekly.rank2.title) + "\n" + processedRanking.weekly.rank2.uri + "\n" 
         + "3." + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.weekly.rank3.title) + "\n" + processedRanking.weekly.rank3.uri + '\n'
+        + moment().tz("Asia/Tokyo").format("YYYY-MM-DD-HH");
 
         tweetPostStatUpdateService.tweetPostStatUpdate(weeklyTweet, "Weekly");
     },
@@ -29,6 +31,7 @@ module.exports = {
         + "1." + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.daily.rank1.title) + "\n" + processedRanking.daily.rank1.uri + "\n" 
         + "2." + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.daily.rank2.title) + "\n" + processedRanking.daily.rank2.uri + "\n" 
         + "3." + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.daily.rank3.title) + "\n" + processedRanking.daily.rank3.uri + '\n'
+        + moment().tz("Asia/Tokyo").format("YYYY-MM-DD-HH");
 
         tweetPostStatUpdateService.tweetPostStatUpdate(dailyTweet, "Daily");
     },
@@ -38,6 +41,7 @@ module.exports = {
         + "1." + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.hourly.rank1.title) + "\n" + processedRanking.hourly.rank1.uri + "\n" 
         + "2." + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.hourly.rank2.title) + "\n" + processedRanking.hourly.rank2.uri + "\n" 
         + "3." + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.hourly.rank3.title) + "\n" + processedRanking.hourly.rank3.uri + '\n'
+        + moment().tz("Asia/Tokyo").format("YYYY-MM-DD-HH");
 
         tweetPostStatUpdateService.tweetPostStatUpdate(hourlyTweet, "Hourly");
     }
--- src/services/tweetTitleTruncaterService.js
+++ src/services/tweetTitleTruncaterService.js
@@ -4,8 +4,8 @@ module.exports = {
     tweetTitleTruncater: (title) => {
         //availble characters: 280 - 3*23 (three links) - 6 (6 new line char) - 2*3 (1. 2. 3. char) - (8*2+1) (title for each ranking) = 182
         //truncate within 30 characters, floor(182 / 3 / 2 ) = 30
-        if (title.length > 30){
-            return title.substring(0, 30);
+        if (title.length > 29){
+            return title.substring(0, 29);
         }else{
             return title;
         }
