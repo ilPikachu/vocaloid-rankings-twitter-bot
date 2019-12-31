e7bac33cc10645db59e4ba814b0910f236ffa3cb
Bob <Bob Bao>
2019-12-30 23:23:09 -0500

    chore: Added 位

--- src/services/post_rankings_services/tweetRankingService.js
+++ src/services/post_rankings_services/tweetRankingService.js
@@ -8,9 +8,9 @@ const tweetTitleTruncaterService = require("../tweetTitleTruncaterService")
 module.exports = {
     monthlyRankingTweet: (processedRanking) => {
         const monthlyTweet = "月間ランキング\n" 
-        + "3. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.monthly.rank3.title) + "\n" + processedRanking.monthly.rank3.uri + '\n'
-        + "2. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.monthly.rank2.title) + "\n" + processedRanking.monthly.rank2.uri + "\n" 
-        + "1. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.monthly.rank1.title) + "\n" + processedRanking.monthly.rank1.uri + "\n" 
+        + "3位. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.monthly.rank3.title) + "\n" + processedRanking.monthly.rank3.uri + '\n'
+        + "2位. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.monthly.rank2.title) + "\n" + processedRanking.monthly.rank2.uri + "\n" 
+        + "1位. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.monthly.rank1.title) + "\n" + processedRanking.monthly.rank1.uri + "\n" 
         + moment().tz("Asia/Tokyo").format("YYYY-MM-DD-HH");
 
         tweetPostStatUpdateService.tweetPostStatUpdate(monthlyTweet, "Monthly");
@@ -18,9 +18,9 @@ module.exports = {
 
     weeklyRankingTweet: (processedRanking) => {  
         const weeklyTweet = "週間ランキング\n" 
-        + "3. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.weekly.rank3.title) + "\n" + processedRanking.weekly.rank3.uri + '\n'
-        + "2. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.weekly.rank2.title) + "\n" + processedRanking.weekly.rank2.uri + "\n" 
-        + "1. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.weekly.rank1.title) + "\n" + processedRanking.weekly.rank1.uri + "\n" 
+        + "3位. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.weekly.rank3.title) + "\n" + processedRanking.weekly.rank3.uri + '\n'
+        + "2位. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.weekly.rank2.title) + "\n" + processedRanking.weekly.rank2.uri + "\n" 
+        + "1位. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.weekly.rank1.title) + "\n" + processedRanking.weekly.rank1.uri + "\n" 
         + moment().tz("Asia/Tokyo").format("YYYY-MM-DD-HH");
 
         tweetPostStatUpdateService.tweetPostStatUpdate(weeklyTweet, "Weekly");
@@ -28,9 +28,9 @@ module.exports = {
 
     dailyRankingTweet: (processedRanking) => { 
         const dailyTweet = "24時間ランキング\n" 
-        + "3. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.daily.rank3.title) + "\n" + processedRanking.daily.rank3.uri + '\n'
-        + "2. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.daily.rank2.title) + "\n" + processedRanking.daily.rank2.uri + "\n" 
-        + "1. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.daily.rank1.title) + "\n" + processedRanking.daily.rank1.uri + "\n" 
+        + "3位. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.daily.rank3.title) + "\n" + processedRanking.daily.rank3.uri + '\n'
+        + "2位. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.daily.rank2.title) + "\n" + processedRanking.daily.rank2.uri + "\n" 
+        + "1位. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.daily.rank1.title) + "\n" + processedRanking.daily.rank1.uri + "\n" 
         + moment().tz("Asia/Tokyo").format("YYYY-MM-DD-HH");
 
         tweetPostStatUpdateService.tweetPostStatUpdate(dailyTweet, "Daily");
@@ -38,9 +38,9 @@ module.exports = {
 
     hourlyRankingTweet: (processedRanking) => {
         const hourlyTweet = "毎時ランキング\n" 
-        + "3. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.hourly.rank3.title) + "\n" + processedRanking.hourly.rank3.uri + '\n'
-        + "2. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.hourly.rank2.title) + "\n" + processedRanking.hourly.rank2.uri + "\n" 
-        + "1. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.hourly.rank1.title) + "\n" + processedRanking.hourly.rank1.uri + "\n" 
+        + "3位. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.hourly.rank3.title) + "\n" + processedRanking.hourly.rank3.uri + '\n'
+        + "2位. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.hourly.rank2.title) + "\n" + processedRanking.hourly.rank2.uri + "\n" 
+        + "1位. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.hourly.rank1.title) + "\n" + processedRanking.hourly.rank1.uri + "\n" 
         + moment().tz("Asia/Tokyo").format("YYYY-MM-DD-HH");
 
         tweetPostStatUpdateService.tweetPostStatUpdate(hourlyTweet, "Hourly");
