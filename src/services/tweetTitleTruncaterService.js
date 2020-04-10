4a19040ed0b87847e9fe4908c9945d44e9a7af7f
Bob <Bob Bao>
2020-04-10 18:02:12 -0400

    chore: add ellipse

--- src/services/tweetTitleTruncaterService.js
+++ src/services/tweetTitleTruncaterService.js
@@ -6,7 +6,7 @@ module.exports = {
         // 280 - 3*23 (three links) - 6 (6 new line chars) - 4*3 ("1位. ", "2位. ", "3位. ") - 11 (max rank title) - 13 (timestamp) = 169
         // truncate within 56 characters, floor(169 / 3) = 56
         if (title.length > 56){
-            return title.substring(0, 56);
+            return title.substring(0, 53) + "...";
         }else{
             return title;
         }
