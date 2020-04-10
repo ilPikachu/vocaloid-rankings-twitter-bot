0f191d079032034ad530c26c95ed8fdd96b87ee9
Bob Bao <baoqiyuan777@gmail.com>
2020-04-10 17:44:30 -0400

    fix: incorrect tweet title truncation calculation.

--- src/services/tweetTitleTruncaterService.js
+++ src/services/tweetTitleTruncaterService.js
@@ -2,10 +2,11 @@
 
 module.exports = {
     tweetTitleTruncater: (title) => {
-        //availble characters: 280 - 3*23 (three links) - 6 (6 new line char) - 2*3 (1. 2. 3. char) - (8*2+1) (title for each ranking) = 182
-        //truncate within 30 characters, floor(182 / 3 / 2 ) = 30
-        if (title.length > 29){
-            return title.substring(0, 29);
+        // Availble characters: 
+        // 280 - 3*23 (three links) - 6 (6 new line chars) - 4*3 ("1位. ", "2位. ", "3位. ") - 11 (max rank title) - 13 (timestamp) = 169
+        // truncate within 56 characters, floor(169 / 3) = 56
+        if (title.length > 56){
+            return title.substring(0, 56);
         }else{
             return title;
         }
