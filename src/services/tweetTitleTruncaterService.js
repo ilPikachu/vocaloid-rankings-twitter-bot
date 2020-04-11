36b4024929345c600a4d6f00a15721d3d084c7a1
Bob Bao <baoqiyuan777@gmail.com>
2020-04-11 01:19:24 -0400

    chore: add hashtags

--- src/services/tweetTitleTruncaterService.js
+++ src/services/tweetTitleTruncaterService.js
@@ -3,10 +3,10 @@
 module.exports = {
     tweetTitleTruncater: (title) => {
         // Availble characters: 
-        // 280 - 3*23 (three links) - 6 (6 new line chars) - 4*3 ("1位. ", "2位. ", "3位. ") - 11 (max rank title) - 13 (timestamp) = 169
-        // truncate within 56 characters, floor(169 / 3) = 56
-        if (title.length > 56){
-            return title.substring(0, 53) + "...";
+        // 280 - 3*23 (three links) - 6 (6 new line chars) - 4*3 ("1位. ", "2位. ", "3位. ") - 11 (max rank title) - 13 (timestamp) - 14(hashtags) = 155
+        // truncate within 51 characters, floor(155 / 3) = 51
+        if (title.length > 51){
+            return title.substring(0, 48) + "...";
         }else{
             return title;
         }
