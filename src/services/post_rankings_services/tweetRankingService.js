291f70507077feb8a4ae765be257513ed2ab90da
Bob Bao <baoqiyuan777@gmail.com>
2020-04-11 01:28:38 -0400

    chore: add newline

--- src/services/post_rankings_services/tweetRankingService.js
+++ src/services/post_rankings_services/tweetRankingService.js
@@ -11,7 +11,7 @@ module.exports = {
         + "3位. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.monthly.rank3.title) + "\n" + processedRanking.monthly.rank3.uri + '\n'
         + "2位. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.monthly.rank2.title) + "\n" + processedRanking.monthly.rank2.uri + "\n" 
         + "1位. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.monthly.rank1.title) + "\n" + processedRanking.monthly.rank1.uri + "\n" 
-        + moment().tz("Asia/Tokyo").format("YYYY-MM-DD-HH")
+        + moment().tz("Asia/Tokyo").format("YYYY-MM-DD-HH") + "\n"
         + "#ボカロ #ボカロランキング";
 
         tweetPostStatUpdateService.tweetPostStatUpdate(monthlyTweet, "Monthly");
@@ -22,7 +22,7 @@ module.exports = {
         + "3位. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.weekly.rank3.title) + "\n" + processedRanking.weekly.rank3.uri + '\n'
         + "2位. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.weekly.rank2.title) + "\n" + processedRanking.weekly.rank2.uri + "\n" 
         + "1位. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.weekly.rank1.title) + "\n" + processedRanking.weekly.rank1.uri + "\n" 
-        + moment().tz("Asia/Tokyo").format("YYYY-MM-DD-HH")
+        + moment().tz("Asia/Tokyo").format("YYYY-MM-DD-HH") + "\n"
         + "#ボカロ #ボカロランキング";
 
         tweetPostStatUpdateService.tweetPostStatUpdate(weeklyTweet, "Weekly");
@@ -33,7 +33,7 @@ module.exports = {
         + "3位. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.daily.rank3.title) + "\n" + processedRanking.daily.rank3.uri + '\n'
         + "2位. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.daily.rank2.title) + "\n" + processedRanking.daily.rank2.uri + "\n" 
         + "1位. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.daily.rank1.title) + "\n" + processedRanking.daily.rank1.uri + "\n" 
-        + moment().tz("Asia/Tokyo").format("YYYY-MM-DD-HH")
+        + moment().tz("Asia/Tokyo").format("YYYY-MM-DD-HH") + "\n"
         + "#ボカロ #ボカロランキング";
 
         tweetPostStatUpdateService.tweetPostStatUpdate(dailyTweet, "Daily");
@@ -44,7 +44,7 @@ module.exports = {
         + "3位. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.hourly.rank3.title) + "\n" + processedRanking.hourly.rank3.uri + '\n'
         + "2位. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.hourly.rank2.title) + "\n" + processedRanking.hourly.rank2.uri + "\n" 
         + "1位. " + tweetTitleTruncaterService.tweetTitleTruncater(processedRanking.hourly.rank1.title) + "\n" + processedRanking.hourly.rank1.uri + "\n" 
-        + moment().tz("Asia/Tokyo").format("YYYY-MM-DD-HH")
+        + moment().tz("Asia/Tokyo").format("YYYY-MM-DD-HH") + "\n"
         + "#ボカロ #ボカロランキング";
 
         tweetPostStatUpdateService.tweetPostStatUpdate(hourlyTweet, "Hourly");
--- src/services/tweetTitleTruncaterService.js
+++ src/services/tweetTitleTruncaterService.js
@@ -3,8 +3,8 @@
 module.exports = {
     tweetTitleTruncater: (title) => {
         // Availble characters: 
-        // 280 - 3*23 (three links) - 6 (6 new line chars) - 4*3 ("1位. ", "2位. ", "3位. ") - 11 (max rank title) - 13 (timestamp) - 14(hashtags) = 155
-        // truncate within 51 characters, floor(155 / 3) = 51
+        // 280 - 3*23 (three links) - 7 (7 new line chars) - 4*3 ("1位. ", "2位. ", "3位. ") - 11 (max rank title) - 13 (timestamp) - 14(hashtags) = 154
+        // truncate within 51 characters, floor(154 / 3) = 51
         if (title.length > 51){
             return title.substring(0, 48) + "...";
         }else{
