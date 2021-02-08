ce7bec96454c211bdd798742aa38c59a546a5202
Bob <baoqiyuan777@gmail.com>
2021-02-07 21:56:19 -0500

    fix: reduce tweet title max length

--- src/services/tweetTitleTruncaterService.js
+++ src/services/tweetTitleTruncaterService.js
@@ -2,8 +2,8 @@
 
 module.exports = {
     tweetTitleTruncater: (title) => {
-        if (title.length > 34){
-            return title.substring(0, 33) + "…";
+        if (title.length > 31){
+            return title.substring(0, 30) + "…";
         }else{
             return title;
         }
